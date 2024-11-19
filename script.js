271e22fe313d573468ec04060eadf1318f327bd5
Simon Kray <162417715+Sarvarbek001@users.noreply.github.com>
2024-11-19 19:24:23 +0500

    IT-soft

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic script to handle form submission (can be customized)
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your message has been sent!');
+});
